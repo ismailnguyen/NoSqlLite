5eb665a58691cb281ae1d77ac55573f13ec2b32d
Ismail NGUYEN <me@nguyenismail.com>
2015-07-22 23:05:17 +0200

    add file reade

---
+++ file.c
@@ -0,0 +1,49 @@
+#include "file.h"
+
+void file_display(char filename)
+{
+   char ch, file_name[25];
+   FILE *fp;
+
+   printf("Enter the name of file you wish to see\n");
+   gets(file_name);
+
+   fp = fopen(file_name,"r"); // read mode
+
+   if( fp == NULL )
+   {
+      perror("Error while opening the file.\n");
+      exit(EXIT_FAILURE);
+   }
+
+   printf("The contents of %s file are :\n", file_name);
+
+   while( ( ch = fgetc(fp) ) != EOF )
+      printf("%c",ch);
+
+   fclose(fp);
+}
+
+void file_write(char content)
+{
+   char ch, file_name[25];
+   FILE *fp;
+
+   printf("Enter the name of file you wish to see\n");
+   gets(file_name);
+
+   fp = fopen(file_name,"r"); // read mode
+
+   if( fp == NULL )
+   {
+      perror("Error while opening the file.\n");
+      exit(EXIT_FAILURE);
+   }
+
+   printf("The contents of %s file are :\n", file_name);
+
+   while( ( ch = fgetc(fp) ) != EOF )
+      printf("%c",ch);
+
+   fclose(fp);
+}
--- hashmap.h
+++ hashmap.h
@@ -20,7 +20,7 @@ typedef struct s_hashmap
     double growth_factor; //taux d’expension longue load factor atteint
     unsigned int initial_slots; //taille initiale de tableau
     unsigned int slots; //taille actuelle du tableau
-    Unsigned int size; //nb entrées dans la map
+    unsigned int size; //nb entrées dans la map
     t_hashmap_entry** entries; //le tableau
 } t_hashmap ;
 
@@ -33,3 +33,12 @@ void * hashmap_get(t_hashmap * map, char* key);
 t_hashmap_keys * hasmap_key(t_hashmap * map);
 void hashmap_expand(t_hashmap * map);
 unsigned int hashmap_hash(char* key, int slots);
+
+//JSON parser
+t_hashmap* JSON_parse(char* string); //Convert a JSON String to a HashMap representation
+char* JSON_stringify(t_hashmap* map); //Convert a HashMap to a JSON String representation
+void* hashmap_traverse(t_hashmap*map, char* path); // Traverse a HashMap according to the given path
+// Ex :  hashmap_traverse(map, 'student.rate')
+void hashmap_put(t_hashmap*map, char* path, void* value); // Traverse a HashMap according to the given path
+// Ex :  hashmap_put(map, 'student.rate', 56);
+//       hashmap_traverse(map, 'student.rate')   56
