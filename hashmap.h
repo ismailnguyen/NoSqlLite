5789dc7c0c2ac39a25ef1be320c9ef4aab3a3867
Ismail NGUYEN <me@nguyenismail.com>
2015-07-23 13:25:01 +0200

    add json prototypes

--- hashmap.h
+++ hashmap.h
@@ -52,6 +52,13 @@ void* hashmap_get(t_hashmap* map, char* key);
 
 t_hashmap_keys* hashmap_keys(t_hashmap* map);
 
-
+//JSON parser
+t_hashmap* JSON_parse(char* string); //Convert a JSON String to a HashMap representation
+char* JSON_stringify(t_hashmap* map); //Convert a HashMap to a JSON String representation
+void* hashmap_traverse(t_hashmap*map, char* path); // Traverse a HashMap according to the given path
+// Ex :  hashmap_traverse(map, 'student.rate')
+void hashmap_put(t_hashmap*map, char* path, void* value); // Traverse a HashMap according to the given path
+// Ex :  hashmap_put(map, 'student.rate', 56);
+//       hashmap_traverse(map, 'student.rate')   56
 
 #endif // HASHMAP_H
