973dc1b9f84892b0ab12bef15046486011d705f0
Ismail NGUYEN <me@nguyenismail.com>
2015-07-23 13:20:14 +0200

    recode hashmap

--- hashmap.c
+++ hashmap.c
@@ -1,112 +1,193 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "hashmap.h"
 
-t_hashmap* hashmap_create(int slots, float load_factor, float grow_factor){
-    t_hashmap* hashmap = (t_hashmap*)malloc(sizeof(t_hashmap));
-    hashmap->slots = slots;
-    hashmap->load_factor = load_factor;
-    hashmap->grow_factor = grow_factor;
-    hashmap->size = 0;
-    hashmap->entries = (t_hashmap_entry**)malloc(sizeof(struct t_hashmap_entry)*slots);
-    return hashmap;
+/*int main()
+{
+	t_hashmap* map = hashmap_create(100, 0.7, 2.1);
+	//t_hashmap_entry* entry = hashmap_entry_create("Bonjour", "hashmap");
+	hashmap_put(map, "Bonjour", "hashmap");
+	hashmap_put(map, "Aurevoir", "prout");
+
+	hashmap_display(map);
+	system("pause");
+	return 0;
+}
+*/
+t_hashmap* hashmap_create(unsigned int slots, double load_factor, double growth_factor)
+{
+	t_hashmap* map = (t_hashmap*)malloc(sizeof(t_hashmap));
+	map->load_factor = load_factor;
+	map->growth_factor = growth_factor;
+	map->initial_slots = slots;
+	map->slots = slots;
+	map->size = 0;
+
+	map->entry = (t_hashmap_entry**)malloc(sizeof(t_hashmap_entry*)*slots);
+
+	int i = 0;
+	for(; i < slots; i++)
+		map->entry[i] = NULL;
+
+	return map;
 }
 
-t_map_entry* map_entry_create(char*k, char*v){
-    t_hashmap_entry* entry = (t_hashmap_entry*)malloc(sizeof(t_hashmap_entry));
-    entry->key = k;
-    entry->value = v;
-    entry->next = NULL;
-    return entry;
+t_hashmap_entry* hashmap_entry_create(char *key, void *value)
+{
+	t_hashmap_entry* entry = (t_hashmap_entry*)malloc(sizeof(t_hashmap_entry));
+	entry->key = key;
+	entry->value = value;
+	entry->next = NULL;
+
+	return entry;
 }
 
-char* indice_result(char* k, int indice){
-    int keyint = (int)k;
-    int result = keyint%indice;
-    char* result_indice = (char*)result;
-    return result_indice;
+void hashmap_display(t_hashmap* map)
+{
+	unsigned int i = 0;
+	t_hashmap_entry* tmp;
+	while(i < map->slots)
+	{
+		tmp = map->entry[i];
+		while(tmp != NULL)
+		{
+			printf("%s -> %s\n", tmp->key, tmp->value);
+			tmp = tmp->next;
+		}
+		i++;
+	}
+}
+unsigned int hashmap_hash(char* key, unsigned int slots)
+{
+	unsigned int hashcode = 1069;
+	while(*key)
+	{
+		hashcode += *(key++);
+	}
+
+	return hashcode % slots;
 }
 
-void hashmap_put(t_hashmap* hashmap, char*k, char* v){
-    if (!hashmap)return;
-    int hashcode = 0;
-    for (int i = 0; i < strlen(k); ++i) {
-        hashcode += k[i];
-    }
-    if (!hashmap->entries[hashcode%hashmap->slots]){
-        hashmap->entries[hashcode%hashmap->slots]= hashmap_entry_create(k,v);
-    }else{
-        t_hashmap_entry* entry = hashmap->entries[hashcode%hashmap->slots];
-        while (&entry){
-            entry=entry->next;
-        }
-        entry=hashmap_entry_create(k,v);
-    }
-}
-char* hashmap_get(t_hashmap* hashmap, char* k){
-    int hashcode = 0;
-    for (int i = 0; k[i]; ++i) {
-        hashcode +=k[i];
-    }
-    t_hashmap_entry* current = hashmap->entries[hashcode%hashmap->slots];
-    while (current){
-        if (strcmp(current->key,k)==0)
-            return current->value;
-        current = current->next;
-    }
-    return NULL;
+/*t_hashmap_keys* hashmap_keys(t_hashmap* map)
+{
+	t_hashmap_keys* hkeys = (t_hashmap_keys*)malloc(sizeof(t_hashmap_keys));
+
+	hkeys->keys = (char**)malloc(sizeof(char*)*hashmap->size);
+
+	unsigned int i = 0;
+	while(i < map->slots)
+	{
+		if(map->entry[i]->key)
+			hkeys->keys[i]
+		i++;
+	}
+}*/
+
+t_hashmap_keys* hashmap_keys(t_hashmap* map)
+{
+	t_hashmap_keys* hkeys = (t_hashmap_keys*)malloc(sizeof(t_hashmap_keys));
+
+	hkeys->keys = (char**)malloc(sizeof(char*)*map->size);
+
+	unsigned int i = 0;
+	unsigned int keyCount = 0;
+	t_hashmap_entry* entry_set;
+
+	for(i = 0; i < map->slots; i++)
+	{
+		entry_set = map->entry[i];
+		while(entry_set)
+		{
+			strcpy(hkeys->keys[keyCount], entry_set->key);
+			keyCount++;
+			hkeys->lenght = keyCount;
+			entry_set = entry_set->next;
+		}
+	}
+
+	return hkeys;
 }
 
-void hashmap_put2(t_hashmap* hashmap, char*k, char*v){
-    int hashcode = 0;
-    for (int i = 0; k[i]; ++i) {
-        hashcode +=k[i];
-    }
-    t_hashmap_entry** current = &hashmap->entries[hashcode%hashmap->slots];
-    while (*current){
-        if (strcmp((*current)->key, k)==0){
-            (*current)->value = v;
-            return;
-        }
-        current = &(*current)->next;
-    }
-    *(current) = hashmap_entry_create(k,v);
+
+void hashmap_put(t_hashmap* map, char* key, void* value)
+{
+	if( map->size >= map->slots * map->load_factor)
+	{
+		//hashmap_expand(map);
+	}
+
+	unsigned int hash = hashmap_hash(key, map->slots);
+	unsigned short found = 0;
+
+	t_hashmap_entry** entry = &(map->entry[hash]);
+
+	while(*entry)
+	{
+		if( 0 == strcmp(key, (*entry)->key))
+		{
+			(*entry)->value = value;
+			found = 1;
+			break;
+		}
+		entry = &(*entry)->next;
+	}
+
+	if(!found)
+	{
+		map->size++;
+		*entry = hashmap_entry_create(key, value);
+	}
 }
 
-void hashmap_delete2(t_hashmap* hashmap, char* k){
-    int hashcode = 0;
-    for (int i = 0; k[i]; ++i) {
-        hashcode +=k[i];
-    }
-    t_hashmap_entry** current = &hashmap->entries[hashcode%hashmap->slots];
-    while (*current){
-        if (strcmp((*current)->key, k)==0){
-            t_hashmap_entry* tmp = *current;
-            *current = (*current)->next;
-            free(tmp);
-        }
-    }
+void* hashmap_get(t_hashmap* map, char* key)
+{
+	unsigned int hash = hashmap_hash(key, map->slots);
+	t_hashmap_entry* entry = map->entry[hash];
+	while(entry)
+	{
+		if(!strcmp(key, entry->key))
+			return entry->value;
+		entry = entry->next;
+	}
+	return NULL;
 }
-void hashmap_delete(t_hashmap* hashmap, char* k){
-    int hashcode = 0;
-    for (int i = 0; k[i]; ++i) {
-        hashcode +=k[i];
-    }
-    t_hashmap_entry* current = hashmap->entries[hashcode%hashmap->slots];
-    t_hashmap_entry* previous = current;
-    if (current&& strcmp(current->key, k)==0){
-        t_hashmap_entry* temp = current;
-        hashmap->entries[hashcode%hashmap->slots]= temp->next;
-        free(temp);
-    }
-    while (current){
-        if (strcmp(current->key, k)==0){
-            previous->next = current->next;
-            free(current);
-        }
-        else {
-            previous = current;
-        }
-        current = previous->next;
-    }
+
+void hashmap_expand(t_hashmap* map)
+{
+	unsigned int i = map->slots;
+	t_hashmap_entry** entry = map->entry;
+
+	map->slots *= map->growth_factor;
+	map->size = 0;
+	map->entry = (t_hashmap_entry**)calloc(map->slots, sizeof(t_hashmap_entry*));
+	t_hashmap_entry *prev, *element;
+	while(i--)														// Parcours de la liste contigue
+	{
+		element = entry[i];
+		while(element)												// Parcours de la liste chaînée
+		{
+			hashmap_put(map, element->key, element->value);			// Recopie l'élément
+			prev = element;
+			element = element->next;
+			free(prev);
+		}
+	}
+	free(entry);
+}
+
+void hashmap_remove(t_hashmap* map, char* key)
+{
+	unsigned int hash = hashmap_hash(key, map->slots);
+	t_hashmap_entry* tmp = NULL;
+	t_hashmap_entry** entry = &map->entry[hash];
+	while(*entry)
+	{
+		if(strcmp(key, (*entry)->key))
+		{
+			tmp = *entry;
+			*entry = (*entry)->next;
+			free(tmp);
+			map->size--;
+			break;
+		}
+		entry = &(*entry)->next;
+	}
 }
--- hashmap.h
+++ hashmap.h
@@ -1,33 +1,57 @@
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef HASHMAP_H
+#define HASHMAP_H
 
 typedef struct s_hashmap_entry
 {
-    char* key;
-    char* value;
-    struct s_hashmap_entry* next;
+	char* key;
+	void* value;
+	struct s_hashmap_entry* next;
 } t_hashmap_entry;
 
 typedef struct s_hashmap
 {
-    int slots; //size of the list
-    int size; //number of elements
-    float load_factor;
-    float grow_factor;
-    t_hashmap_entry** entries;
+	double load_factor;				// Taux de remplissage maximum
+	double growth_factor;			// Taux d'expansion lorsque remplissage max atteind
+	unsigned int initial_slots;		// Taille initial du tableau
+	unsigned int slots;				// Taille actuelle du tableau
+	unsigned int size;				// Nombre d'entrée dans la map
+	t_hashmap_entry** entry;		// Tableau
 } t_hashmap;
 
+typedef struct s_hashmap_keys
+{
+	char** keys;
+	unsigned int lenght;
+}t_hashmap_keys;
 
-t_hashmap* hashmap_create(int slots, float load_factor, float grow_factor);
-t_hashmap_entry* hashmap_entry_create(char* k, char* v);
-char* hashmap_get(t_hashmap* map, char* k);
-void hashmap_remove(t_hashmap* map, char* k);
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-//JSON parser
-t_hashmap* JSON_parse(char* string); //Convert a JSON String to a HashMap representation
-char* JSON_stringify(t_hashmap* map); //Convert a HashMap to a JSON String representation
-void* hashmap_traverse(t_hashmap*map, char* path); // Traverse a HashMap according to the given path
-// Ex :  hashmap_traverse(map, 'student.rate')
-void hashmap_put(t_hashmap*map, char* path, void* value); // Traverse a HashMap according to the given path
-// Ex :  hashmap_put(map, 'student.rate', 56);
-//       hashmap_traverse(map, 'student.rate')   56
+// Création la hashmap : Initialiser la structure et affecter NULL à toutes les valeurs du tableau entry
+t_hashmap* hashmap_create(unsigned int slots, double load_factor, double growth_factor);
+
+void hashmap_display(t_hashmap* map);
+
+// Ajout d'un élément
+t_hashmap_entry* hashmap_entry_create(char *key, void *value);
+
+// Fonction de hashage
+unsigned int hashmap_hash(char* key, unsigned int slots);
+
+// Si size < slots * load_factor
+// Calcul du hash et de l'indice
+// Si clé existe :
+//		Si non : Mise à jour de la valeur
+//		Si non : Ajout de l'entrée dans la liste chainée
+void hashmap_put(t_hashmap* map, char* key, void* value);
+
+void hashmap_remove(t_hashmap* map, char* key);
+
+void* hashmap_get(t_hashmap* map, char* key);
+
+t_hashmap_keys* hashmap_keys(t_hashmap* map);
+
+
+
+#endif // HASHMAP_H
